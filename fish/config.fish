e696ce3dbed9cc82b161e15245f6c43711340180
Matt Keeter <matt.j.keeter@gmail.com>
2016-04-18 11:14:40 -0400

    Update OMF

--- fish/config.fish
+++ fish/config.fish
@@ -1,34 +1,33 @@
-# Path to your oh-my-fish.
-set fish_path $HOME/.oh-my-fish
-
-# Theme
-set fish_theme robbyrussell
-
-# All built-in plugins can be found at ~/.oh-my-fish/plugins/
-# Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
-# Enable plugins by adding their name separated by a space to the line below.
-set fish_plugins theme z brew git-flow tmux vi-mode
-
-# Load oh-my-fish configuration.
-. $fish_path/oh-my-fish.fish
-
-# Make virtualenv work
-eval (python -m virtualfish)
-
 # Use vim as our default editor
 set --export EDITOR vim
 alias make="make -j8"
+
+# Make virtualenvs work
+eval (python -m virtualfish)
 
 # Set SHELL=fish (so tmux uses fish instead of zsh)
 set --export SHELL (which fish)
 
 # Useful aliases
 alias miniterm="python -m serial.tools.miniterm"
-alias org="vim ~/org"
 alias w="vim ~/wiki/index.md"
 
 # Customize PATH
 set fish_user_paths ~/code/kokopelli                                  \
                     ~/code/gcc-arm-none-eabi-4_8-2013q4/bin           \
                     ~/mit/cba/repos/kokompe/dev/mod/bin               \
-                    ~/Library/Haskell/bin
+                    ~/Library/Haskell/bin                             \
+                    /Developer/NVIDIA/CUDA-7.5/bin
+
+set -g Z_SCRIPT_PATH /usr/local/etc/profile.d/z.sh
+
+################################################################################
+
+# Path to Oh My Fish install.
+set -gx OMF_PATH "/Users/mkeeter/.local/share/omf"
+
+# Customize Oh My Fish configuration path.
+set -gx OMF_CONFIG "/Users/mkeeter/.dotfiles/omf"
+
+# Load oh-my-fish configuration.
+source $OMF_PATH/init.fish
