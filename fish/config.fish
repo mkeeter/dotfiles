fc0ef258b8c3b833d844e41af06590293363ba68
Matt Keeter <matt.j.keeter@gmail.com>
2018-05-10 16:31:27 -0400

    Hello from new machine

--- fish/config.fish
+++ fish/config.fish
@@ -3,7 +3,7 @@ set --export EDITOR vim
 alias make="make -j8"
 
 # Make virtualenvs work
-eval (python -m virtualfish)
+eval (python3 -m virtualfish)
 
 # Set SHELL=fish (so tmux uses fish instead of zsh)
 set --export SHELL (which fish)
@@ -15,26 +15,6 @@ alias weather="curl wttr.in/somerville,ma"
 alias fix-inkscape='wmctrl -r Inkscape -e 0,2560,1440,1200,700'
 
 # Customize PATH
-set fish_user_paths ~/code/kokopelli                                  \
-                    ~/code/gcc-arm-none-eabi-5_4-2016q2/bin           \
-                    ~/mit/cba/repos/kokompe/dev/mod/bin               \
-                    ~/.local/bin                                      \
-                    /Developer/NVIDIA/CUDA-7.5/bin                    \
-                    ~/.cargo/bin                                      \
-                    /usr/local/texlive/2014/bin/x86_64-darwin/ 
-
 set -g Z_SCRIPT_PATH /usr/local/etc/profile.d/z.sh
 
 ################################################################################
-
-# Path to Oh My Fish install.
-set -gx OMF_PATH "/Users/mkeeter/.local/share/omf"
-
-# Customize Oh My Fish configuration path.
-set -gx OMF_CONFIG "/Users/mkeeter/.dotfiles/omf"
-
-# Load oh-my-fish configuration.
-source $OMF_PATH/init.fish
-
-# Configure rbenv
-status --is-interactive; and source (rbenv init -|psub)
