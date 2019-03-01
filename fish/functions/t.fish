41903ad4b6df95d762004ae5181356e48eead243
Matt Keeter <matt.j.keeter@gmail.com>
2019-03-01 10:43:06 -0500

    Improve t when called without arguments

--- fish/functions/t.fish
+++ fish/functions/t.fish
@@ -1,3 +1,7 @@
 function t
-	z $argv; and tmux new-session -s $argv; or tmux attach -t $argv
+    if count $argv > /dev/null
+        z $argv; and tmux new-session -s $argv 2> /dev/null; or tmux attach -t $argv
+    else
+        tmux list-sessions
+    end
 end
