8784b7c1838906d87eb327f3c75e2853e6383191
Matt Keeter <matt.j.keeter@gmail.com>
2019-10-09 08:45:05 -0400

    Move Mac-specific config into a separate file

--- fish/config.fish
+++ fish/config.fish
@@ -2,26 +2,22 @@
 set --export EDITOR vim
 alias make="make -j8"
 
-# Make virtualenvs work
-eval (python3 -m virtualfish)
-
 # Set SHELL=fish (so tmux uses fish instead of zsh)
 set --export SHELL (which fish)
 
 # Useful aliases
-alias miniterm="python -m serial.tools.miniterm"
-alias w="vim ~/wiki/index.md"
 alias weather="curl --silent wttr.in/somerville,ma|grep -v wttr"
-alias fix-inkscape='wmctrl -r Inkscape -e 0,2560,1440,1200,700'
-alias git-merge-log='git log --format=\'format:# %cd %H %s\' --date=short --first-parent --reverse'
 
 # Configure Z for fast jumping
 set -g Z_SCRIPT_PATH /usr/local/etc/profile.d/z.sh
 
 # Customize PATH
-set fish_user_paths /usr/local/bin ~/.cargo/bin ~/go/bin
+set fish_user_paths /usr/local/bin ~/.cargo/bin ~/go/bin /usr/local/texlive/2014/bin/x86_64-darwin
 
+# Reset the greeting
 set fish_greeting
 
-# Configure rbenv
-status --is-interactive; and source (rbenv init -|psub)
+switch (uname)
+    case Darwin
+        source ~/.config/fish/config.darwin.fish
+end
