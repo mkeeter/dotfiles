6f174b40d2110d2ec2a8ff007bb50d1d3c495277
Matt Keeter <matt.j.keeter@gmail.com>
2021-11-05 09:26:33 -0400

    Add note about invalidation

--- nvim/init.lua
+++ nvim/init.lua
@@ -29,6 +29,7 @@ vim.g.mapleader = ","
 vim.cmd[[cd %:p:h]]
 
 -- Plugin cache must be loaded before any other plugins
+-- (TODO: remove when https://github.com/neovim/neovim/pull/15436 is merged)
 require('impatient')
 require('packer_compiled')
 
