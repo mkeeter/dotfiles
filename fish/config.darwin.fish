a542c549fdeffd7687ea2df26084452043084a43
Matt Keeter <matt.j.keeter@gmail.com>
2021-11-05 09:49:15 -0400

    Complete the switch to nvim

--- fish/config.darwin.fish
+++ fish/config.darwin.fish
@@ -2,7 +2,7 @@ set --export HOMEBREW_GITHUB_API_TOKEN (cat ~/.config/brew/token)
 alias fix-inkscape='wmctrl -r Inkscape -e 0,2560,1440,1200,700'
 
 # Quick access to personal wiki
-alias w="vim ~/wiki/index.md"
+alias w="nvim ~/wiki/index.md"
 
 # Configure rbenv
 status --is-interactive; and source (rbenv init -|psub)
