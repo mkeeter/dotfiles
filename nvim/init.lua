8ce1995790657b67274d05fc59c839d8eba6b5d9
Matt Keeter <matt@oxide.computer>
2022-01-04 10:28:58 -0500

    Use 'q' to close quickfix window

--- nvim/init.lua
+++ nvim/init.lua
@@ -34,6 +34,9 @@ vim.g.mapleader = ","
 -- Set current directory
 vim.cmd[[cd %:p:h]]
 
+-- Bind 'q' to close a quickfix window
+vim.cmd[[autocmd BufWinEnter quickfix nnoremap <buffer> q :cclose<CR>]]
+
 -- Plugin cache must be loaded before any other plugins
 -- (TODO: remove when https://github.com/neovim/neovim/pull/15436 is merged)
 require('impatient')
