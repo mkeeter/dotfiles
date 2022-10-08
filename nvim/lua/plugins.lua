12e5f5eeea0123ebdcb7497f8d528f960d81d02b
Matt Keeter <matt.j.keeter@gmail.com>
2022-10-08 18:46:42 -0400

    Fix typo

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -20,7 +20,7 @@ return require('packer').startup{function()
   }
 
   use {
-    'ggandor/leap.nvim'
+    'ggandor/leap.nvim',
     config = function()
       require('leap').set_default_keymaps()
     end
