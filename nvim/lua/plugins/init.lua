056434f74692e56235ad55978690e367f939332c
Matt Keeter <matt.j.keeter@gmail.com>
2024-05-31 11:39:38 -0400

    Remove now-default mappings

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -96,14 +96,6 @@ return {
     'neovim/nvim-lspconfig',
     config = function()
       local bufopts = { noremap=true, silent=true }
-      vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-      vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-      vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
-      vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-      vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-      vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
       vim.diagnostic.config({
         virtual_text = false,
         signs = true,
