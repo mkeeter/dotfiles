28294e43f556e2fda142dfd5a82235235545bf7f
Matt Keeter <matt@oxide.computer>
2025-08-13 16:00:36 -0400

    More cleanup

--- fish/functions/z.fish
+++ fish/functions/z.fish
@@ -2,7 +2,7 @@ function z -d "jump around"
   set -l target (zipzap --quiet find $argv)
   set -l Z_STATUS $status
   if test $Z_STATUS -eq 0; and test -n "$target"
-    builtin cd $target
+    builtin cd "$target"
   end
   return $Z_STATUS
 end
